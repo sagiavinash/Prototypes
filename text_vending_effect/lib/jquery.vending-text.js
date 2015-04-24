13c3b21e2fe9bba089b7e0e0b908d2c9676ea4d1
sagiavinash <sagiavinash@github.com>
2015-04-24 18:44:27 +0530

    converted functionality into jquery plugin

--- text_vending_effect/lib/jquery.vending-text.js
+++ text_vending_effect/lib/jquery.vending-text.js
@@ -1,48 +1,82 @@
-$(document).ready(function(){
+;
+(function($, window, document, undefined) {
+    "use strict";
+
 	$.fn.extend({
-	  reflow : function(){
-	    var trigger = $("body").offset().top;
-	    return this;
-	  },
-	  run: function(fn){
-	  	fn();
-	  	return this;
-	  }
+		reflow : function(){
+			var trigger = $("body").offset().top;
+			return this;
+		},
+		run: function(fn){
+			fn();
+			return this;
+		}
 	});
 
-	$(".btn-slide").on("click", function(){
-		if($(".vending-text").data("play") == false) return;
-		$(".vending-text").data("play", false);
-		var $vendingList = $(".vending-text"),
-			index = {}, $current, $next, state = {}, dir;
-		dir = $(this).hasClass("slide-up") ? "-1" : "1";
-		
-		index.current = $vendingList.index($vendingList.filter(".active"));
-		var _val = index.current + (+dir);
-		index.next = ((0 <= _val) && (_val < $vendingList.length)) ? _val : ((_val < 0) ? ($vendingList.length - 1) : 0);
-		
-		state = {
-			"-1" : { current : "hide-top", next : "hide-bottom" },
-			"1" : { current : "hide-bottom", next : "hide-top" }
-		};
+	function vendingText(element, options) {
+        this.element = element;
+        this.defaults = {};
+        this.options = $.extend({}, this.defaults, options);
+        this.init();
+    }
 
-		$current = $vendingList.eq(index.current);
-		$next = $vendingList.eq(index.next);
-		
-		$current
-			.addClass("animate " + state[dir].current)
-			.removeClass("active")
-			.run(function(){
-				setTimeout(function(){
-					$current.addClass("hide");
-					$(".vending-text").data("play", true);
-				}, 400);
-			});
-		$next
-			.removeClass("animate " + state[dir].current)
-			.addClass(state[dir].next)
-			.reflow()
-			.addClass("active animate")
-			.removeClass("hide " + state[dir].next);
-	});
-});
+    vendingText.prototype = {
+    	init : function(){
+    		var $elem = $(this.element);
+    	},
+    	slide : function($btn){
+    		var $elem = $(this.element);
+    		if($elem.data("play") == false) return;
+			$elem.data("play", false);
+			var $vendingList = $elem.find(".vending-list-item"),
+				index = {}, $current, $next, state = {}, dir;
+			dir = $elem.data("slide") == "up" ? "-1" : "1";
+			
+			index.current = $vendingList.index($vendingList.filter(".vending-active"));
+			var _val = index.current + (+dir);
+			index.next = ((0 <= _val) && (_val < $vendingList.length)) ? _val : ((_val < 0) ? ($vendingList.length - 1) : 0);
+			
+			state = {
+				"-1" : { current : "vending-hide-top", next : "vending-hide-bottom" },
+				"1" : { current : "vending-hide-bottom", next : "vending-hide-top" }
+			};
+
+			$current = $vendingList.eq(index.current);
+			$next = $vendingList.eq(index.next);
+			
+			$current
+				.addClass("vending-animate " + state[dir].current)
+				.removeClass("vending-active")
+				.run(function(){
+					setTimeout(function(){
+						$current.addClass("vending-hide");
+						$elem.data("play", true);
+					}, 400);
+				});
+			$next
+				.removeClass("vending-animate " + state[dir].current)
+				.addClass(state[dir].next)
+				.reflow()
+				.addClass("vending-active vending-animate")
+				.removeClass("vending-hide " + state[dir].next);	
+    	}
+    }
+    $.fn.vendingText = function(options) {
+        return this.each(function() {
+            $.data(this, "vendingText", new vendingText(this, options));
+            if (!$.data(this, "vendingText")) {
+                // preventing against multiple instantiations
+                $.data(this, "vendingText", new vendingText(this, options));
+            } else {
+                var vendingTextObj = $.data(this, "vendingText");
+                // checking if option is a valid function name
+                if (typeof options === "string" && vendingTextObj[options]) {
+                    vendingTextObj[options].call(vendingTextObj);
+                } else if (typeof options === "object") {
+                    // if the option is object extending it with initalized object
+                    vendingTextObj.options = $.extend({}, vendingTextObj.options, options);
+                }
+            }
+        });
+    };
+})(jQuery, window, document);
